cee5b3b9abf1f9c7d277d21724b3a0a750671124
danielczweig <danielczweig@gmail.com>
2022-09-16 22:40:18 -0700

    added explain analyze to queries to get query times

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 // get all products
 router.get('/', (req, res) => {
-  const query = 'SELECT * FROM products limit 5'
+  const query = 'EXPLAIN ANALYZE SELECT * FROM products limit 5'
   const params = []
   db.query(query, params, (err, result) => {
     if (err) {
@@ -19,7 +19,8 @@ router.get('/', (req, res) => {
 // get specific product data
 router.get('/:product_id', (req, res) => {
   const query =
-    `SELECT "p".*, f.features
+    `EXPLAIN ANALYZE
+    SELECT "p".*, f.features
     FROM "products" as "p"
     LEFT JOIN (
       SELECT "features"."product_id", json_agg(json_build_object('feature', features.feature, 'value', features.value)) as features
@@ -41,7 +42,8 @@ router.get('/:product_id', (req, res) => {
 // get styles for specific product id
 router.get('/:product_id/styles', (req, res) => {
   const query =
-  `SELECT styles.product_id, "results".results
+  `EXPLAIN ANALYZE
+  SELECT styles.product_id, "results".results
   FROM "styles"
   LEFT JOIN (
     SELECT styles.id, json_agg(json_build_object('style_id', styles.id, 'name', styles.name, 'original_price', styles.original_price, 'sale_price', styles.sale_price, 'default?', styles.default_style, 'photos', "photos".photos, 'skus', "skus".skus)) as results
@@ -72,7 +74,7 @@ router.get('/:product_id/styles', (req, res) => {
 
 // get related products for specific product id
 router.get('/:product_id/related', (req, res) => {
-  const query = `SELECT json_agg(related.related_product_id) FROM related WHERE current_product_id = $1`
+  const query = `EXPLAIN ANALYZE SELECT json_agg(related.related_product_id) FROM related WHERE current_product_id = $1`
   const params = [req.params.product_id]
   db.query(query, params, (err, result) => {
     if(err) {
