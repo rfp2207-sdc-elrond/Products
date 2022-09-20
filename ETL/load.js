07b8fe28eec7f950d20978f3616eaefdf70b8cb2
danielczweig <danielczweig@gmail.com>
2022-09-20 12:21:13 -0700

    added local load tests file

--- ETL/load.js
+++ ETL/load.js
@@ -1,4 +1,4 @@
-// type the following script into the command line to copy the products table from sdc-products-staging to sdc-products (make sure products in sdc-products is empty before running command):
+// type the following script into the command line to copy the products table from sdc-products-staging to sdc-products (make sure products in sdc-products is empty):
 
 // pg_dump -a -t products sdc-products-staging | psql sdc-products
 
@@ -26,7 +26,7 @@ const db = pgp(credentials);
 
 const createIndex = async() => {
   await db.any(
-    `CREATE UNIQUE INDEX id_idx ON products (id);`
+    `CREATE UNIQUE INDEX id_idx ON products(id);`
   )
 };
 
