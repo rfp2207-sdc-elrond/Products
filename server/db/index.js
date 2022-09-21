01a6d518487214ac57855365e7ac4f7022cf2537
danielczweig <danielczweig@gmail.com>
2022-09-20 21:39:54 -0700

    made updates to connect to ec2 instance of database

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,7 @@ const credentials = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
 }
 
